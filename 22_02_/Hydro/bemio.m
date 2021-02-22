139e021a996d3d735dad85aeac8ccb17cce1543a
reazner <77896616+reazner@users.noreply.github.com>
2021-02-22 13:48:25 -0800

    Add files via upload

---
+++ 22_02_/Hydro/bemio.m
@@ -0,0 +1,12 @@
+clc; clear all; 
+close all;
+hydro = struct();
+
+%hydro = Read_AQWA(hydro,'WaVe.AH1','WaVe.LIS');
+% they have the exact same patterns, just the magnitude is twiceish COB=fullx2
+hydro = Read_AQWA(hydro,'Tech2.AH1','Tech2.LIS');
+hydro = Radiation_IRF(hydro,15,[],[],[],[]);
+hydro = Radiation_IRF_SS(hydro,[],[]);
+hydro = Excitation_IRF(hydro,15,[],[],[],[]);
+Write_H5(hydro)
+Plot_BEMIO(hydro)
